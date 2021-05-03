f351fa25c966389881ae38b989f2df5bd67bb2b7
Sexpistols <Sexpistols@DESKTOP-NV4A86E>
2021-05-03 17:12:23 +0900

    Casting

--- JavaEx/src/com/javaex/basics/Types.java
+++ JavaEx/src/com/javaex/basics/Types.java
@@ -2,18 +2,17 @@ package com.javaex.basics;
 
 //	기본 타입 예제
 public class Types {
-	
+
 	public static void main(String[] args) {
 //		integerTypes();
 //		floatTypes();
 //		booleanEx();
 //		charEx();
-		constantEx();
 //		constantEx();
 //		implicitCasting();
 		explicitCasting();
 	}
-
+	
 	//	명시적 형변환
 	//	표현의 범위 넓을 자료형 -> 좁은 자료형으로 변환
 	//	자료의 유실 가능, 개발자가 명시적으로 변환
@@ -21,15 +20,15 @@ public class Types {
 		byte b;	//	1바이트 정수형
 		int i = 2021;	//	4바이트 정수형
 		float f = 123.456f;	//	4바이트 실수형
-
+		
 		System.out.println(Integer.toBinaryString(i));
-
+		
 		b = (byte)i;	//	강제로 캐스팅
 		System.out.println("int " + i + " -> byte " + b);
-
+		
 		i = (int)f;
 		System.out.println("float " + f + " -> int " + i);
-
+		
 	}
 	//	암묵적 형변환
 	//	표현의 범위 좁은 자료형 -> 넓은 자료형으로 변환
