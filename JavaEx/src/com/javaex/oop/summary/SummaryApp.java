c15a18842e0880c8494c23f55cace1d5dbb58e8d
bit <bit@DESKTOP-60QQ7IK>
2021-05-07 16:44:10 +0900

    OOP 정리

--- JavaEx/src/com/javaex/oop/summary/SummaryApp.java
+++ JavaEx/src/com/javaex/oop/summary/SummaryApp.java
@@ -3,26 +3,26 @@ package com.javaex.oop.summary;
 public class SummaryApp {
 	
 	static KungfuSkill dojang[];
-
+	
 	public static void main(String[] args) {
-		Human h1 = new TheOne("고길동", 50);
+		Human h1 = new Human("고길동", 50);
 		Human h2 = new TheOne("네오", 48);
 		Panda p1 = new Panda("핑", 30);
-		Panda p2 = new Panda("포", 15);
+		Panda p2 = new KungfuPanda("포", 15);
 		
 		fight(h1);
 		fight(h2);
 		fight(p1);
 		fight(p2);
 		
-		fight(h1);
-		fight(h2);
-		fight(p1);
-		fight(p2);
+		fly(h1);
+		fly(h2);
+		fly(p1);
+		fly(p2);
 		
 		//	Kungfu 할 수 있는 객체를 dojang
 		dojang = new KungfuSkill[] {
-//				(KungfuSkill)h1,	//	-> KungfuSkill을 구현하지 않음
+//				(KungfuSkill)h1,	//	-> KungfuSill을 구현하지 않음
 				(KungfuSkill)h2,
 //				(KungfuSkill)p1,
 				(KungfuSkill)p2
